a19eb8f6794d2df63bd87f1976ad0cdb4e0983bf
Thaumas/ <thomas.pln@protonmail.com>
2021-10-13 09:53:37 +0200

    JS begin connect

--- sport_track_web_app_js/web_app/app.js
+++ sport_track_web_app_js/web_app/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var users = require('./routes/users');
+var connect = require('./routes/connect');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', users);
+app.use('/connect', connect);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
